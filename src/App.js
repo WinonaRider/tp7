5ad3b23440145a3b9b462c7cc52ac1edae1e0c0b
unknown <marchioripedro1@gmail.com>
2023-04-20 05:21:33 -0300

    update all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { Divider, Input, Alert, TextField} from '@mui/material';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const [wordCount, setWordCount] = useState(0);
+
+  const [charCount, setCharCount] = useState(0);
+
+  const changeHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    const words = text.split(' ');
+
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+    setCharCount(text.length);
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Divider className='container'>
+      
+      <Divider>
+        <br></br>
+        <TextField
+        className='bar'
+          style={{background: "#ddd0c2"}}
+          maxLength="100"
+          value={text}
+          onChange={changeHandler}
+          placeholder='Que estas pensando?'
+        ></TextField>
+       
+      <p>{text}</p>
+      
+        <Divider className="progress">
+          <form>{100 - text.length} caracteres restantes.</form>
+          <Divider>
+              {(() => {
+                if (100 - text. length == 0) {
+                  
+                  return (
+                    Alert("alcanzo el limite de caracteres")
+                    
+                  )
+                  
+                }
+              })()}
+          </Divider>
+
+        <Divider>
+          <p className='word-count'>Palabras: {wordCount}</p>
+          <p className='char-count'>Caracteres: {charCount}</p>
+        </Divider>
+      </Divider>
+    </Divider>
+    </Divider>
   );
 }
 
